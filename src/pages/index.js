a85b375517dc177276a3bddd51e696740de42530
rt-inconnect <r.toshpulatov@gmail.com>
2020-12-04 01:36:01 +0500

    Added feature image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,12 @@
 import React from "react"
-import { Container } from "../components"
+import { Container, FeatureImage } from "../components"
 
 export const IndexPage = () => {
-  return <Container></Container>
+  return (
+    <Container>
+      <FeatureImage />
+    </Container>
+  )
 }
 
 export default IndexPage
